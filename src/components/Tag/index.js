2ca60ce55d1b1037015f2d872f51904fe27a877e
HermersonDev <ne.hermerson@gmail.com>
2019-12-26 23:14:10 -0300

    Add tag styles

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 import './styles.css';
 
-const Tag = () => (
-	<p>Tag</p>
+const Tag = ({ tagValue = 'comedy', moreTag = false, more = 1 }) => (
+	<div id="tags">
+		<div className="tag">{tagValue}</div>
+		{ moreTag && 
+			<div className="tag circle">+{more}</div>
+		}
+	</div>
 );
 
 export default Tag;
