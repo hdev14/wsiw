b8d251ea5d4dcbc83bf4e5d7d3a0dc1a75fef8e2
HermersonDev <ne.hermerson@gmail.com>
2019-12-26 22:13:55 -0300

    Add empty card

--- src/components/EmptyCard/index.js
+++ src/components/EmptyCard/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './styles.css';
 
+//components
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
 const EmptyCard = () => (
-	<p> Empty Card</p>
+	<div id="empty-card">
+		<FontAwesomeIcon id="plus" icon={faPlus} />
+	</div>
 );
 
 export default EmptyCard;
