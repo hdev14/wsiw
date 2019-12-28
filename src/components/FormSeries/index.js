711935d3cd187d9b7655301b88cdd783ceb06244
HermersonDev <ne.hermerson@gmail.com>
2019-12-27 23:01:27 -0300

    Add form

--- src/components/FormSeries/index.js
+++ src/components/FormSeries/index.js
@@ -4,34 +4,48 @@ import './styles.css';
 
 //compenents
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
 import {
 	Button,
+	Form,
+	FormGroup,
+	Input,
 	Modal,
 	ModalHeader,
 	ModalBody,
-	ModalFooter
+	ModalFooter,
 } from 'reactstrap';
 
 const FormSeries = ({ modalTitle = 'New Series'}) => {
 
+	const [edit, setEdit] = useState(faPen);
 	const [modal, setModal] = useState(false);
 
-	const toggle = () => setModal(!modal);
+	const toggle = () => {
+		setModal(!modal);
+		setEdit( (edit == faPen) ? faTimes : faPen );
+	}
 
 	return (
 		<div>
 			<button id="edit" onClick={toggle}>
-				<FontAwesomeIcon icon={faPen}/>
+				<FontAwesomeIcon icon={edit}/>
 			</button>
 			<Modal centered id="form-series-modal" isOpen={modal} toggle={toggle}>
 				<ModalHeader>{modalTitle}</ModalHeader>
 				<ModalBody>
-					Form
+					<Form id="series-form">
+						<FormGroup>
+							<Input type="text" placeholder="Title" />		
+						</FormGroup>
+						<FormGroup>
+							<Input id="ta" type="textarea" placeholder="Description" />		
+						</FormGroup>
+					</Form>
+					
 				</ModalBody>
 				<ModalFooter>
-					<Button id="close" onClick={toggle}>Cancel</Button>
-					<Button id="save">Save</Button>
+					<Button type="submit" form="series-form" id="save" className="btn-block">Save</Button>
 				</ModalFooter>
 			</Modal>
 		</div>
