4d43594d680acd0f64ba9adbdf00edf74b9e3064
HermersonDev <ne.hermerson@gmail.com>
2019-12-29 20:48:39 -0300

    EmptyCardGenre styles

--- src/components/EmptyCardGenre/index.js
+++ src/components/EmptyCardGenre/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './styles.css';
 
+//components
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
 const EmptyCardGenre = () => (
-	<p>empty card genre</p>
+	<div id="empty-card-genre">
+		<FontAwesomeIcon id="icon" icon={faPlus} />
+	</div>
 );
 
 export default EmptyCardGenre;
