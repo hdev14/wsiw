d14cccb7b686772a1961d65c50c8e50b6b9fa1b1
HermersonDev <ne.hermerson@gmail.com>
2020-01-03 21:23:05 -0300

    add styles modal-genre-modal

--- src/components/EmptyCardGenre/index.js
+++ src/components/EmptyCardGenre/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
 //components
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import {
+	Button,
+	Form,
+	FormGroup,
+	Input,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	ModalFooter
+} from 'reactstrap';
+const EmptyCardGenre = () => {
 
-const EmptyCardGenre = () => (
-	<div id="empty-card-genre">
-		<FontAwesomeIcon id="icon" icon={faPlus} />
-	</div>
-);
+	const [modal, setModal] = useState(true);
+
+	const toggle = () => setModal(!modal);
+
+	return (
+		<div>
+			<div id="empty-card-genre" onClick={toggle}>
+				<FontAwesomeIcon id="icon" icon={faPlus} />
+			</div>
+			<Modal id="form-genre-modal" centered isOpen={modal} toggle={toggle}>
+				<ModalHeader toggle={toggle}>New genre</ModalHeader>
+				<ModalBody>
+					<Form id="form-modal-genre">
+						<FormGroup>
+							<Input type="text" placeholder="Genre" />
+						</FormGroup>
+					</Form>
+				</ModalBody>
+				<ModalFooter>
+					<Button id="save" form="form-modal-genre" className="btn-block">Save</Button>
+				</ModalFooter>
+			</Modal>
+		</div>
+
+	);
+}
 
 export default EmptyCardGenre;
